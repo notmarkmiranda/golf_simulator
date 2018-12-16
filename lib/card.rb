dcbaa0123863d4694644d4200ecd1177ac0f79a6
notmarkmiranda <notmarkmiranda@gmail.com>
2018-12-16 13:23:21 -0700

    Complete card and card spec

---
+++ lib/card.rb
@@ -0,0 +1,20 @@
+class Card
+  attr_reader :number, :suit
+
+  VALUES = {
+    'JO' => -3,
+    'A' => 1,
+    'K' => 0,
+    'Q' => 10,
+    'J' => 10
+  }
+
+  def initialize(number:, suit: nil)
+    @number = number
+    @suit = suit
+  end
+
+  def value
+    VALUES[number] ? VALUES[number] : number.to_i
+  end
+end
